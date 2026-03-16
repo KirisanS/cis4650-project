2ceadb6c8e7b70cc17b4ba2ade66f43a6d6effa2
Lauren Kardosh <lkardosh@uoguelph.ca>
2026-03-15 21:30:58 -0400

    updated main to create output file

--- src/CM.java
+++ src/CM.java
@@ -1,16 +1,17 @@
 import java.io.*;
 import absyn.*;
 
-class CM {
+class CM { 
   // public final static boolean SHOW_TREE = true;
   // public final static boolean SHOW_SEMANTIC = true;
   public static void main(String argv[]) {
 
     try {
-      // not showing without -c arg
+      //used to determine what output we want to display
       boolean saveTree = false;
       boolean saveSymbolsAndTree = false;
-      String filename = null;
+      String inputFile = null;
+      String baseFile = null;
 
       //allows fof either arg input order
       for(int n=0; n < argv.length; n++){
@@ -20,41 +21,66 @@ class CM {
         else if(argv[n].equals("-s")){
           saveSymbolsAndTree = true;
         }
-        else{
-          filename = argv[n];
+        else {
+          inputFile = argv[n];
         }
       }
 
-      if (filename == null) {
-        System.err.println("Error: no input file specified");
+      if (inputFile == null) {
+        System.err.println("Error: no input file given");
         return;
       }
-        //parses the file
-        parser p = new parser(new Lexer(new FileReader(filename)));
-        Absyn result = (Absyn)(p.parse().value);
-        //displays syntax tree and semantic analysis
-        if (saveSymbolsAndTree && result != null) {
-          System.out.println("The abstract syntax tree is:");
-          AbsynVisitor visitor = new ShowTreeVisitor();
-          result.accept(visitor, 0);
+
+      if (!saveTree && !saveSymbolsAndTree) {
+        System.err.println("Error: -s or -a required to run syntax tree and/or semantic analysis :(");
+        return;
+      }
+
+      if (inputFile.endsWith(".cm")) {
+        baseFile = inputFile.substring(0, inputFile.length() - 3);
+      } else {
+        baseFile = inputFile;
+      }
+
+      //parses the file
+      parser p = new parser(new Lexer(new FileReader(inputFile)));
+      Absyn result = (Absyn)(p.parse().value);
+
+      if (result == null) {
+        System.err.println("Syntax errors found, program terminated.");
+        return;
+      }
+
+      PrintStream console = System.out;
+        
+      //displays syntax tree and semantic analysis
+      if (saveSymbolsAndTree) {
+        PrintStream outputFile = new PrintStream(new FileOutputStream(baseFile + ".sym"));
+        System.setOut(outputFile);
+        System.out.println("The abstract syntax tree is:");
+        AbsynVisitor visitor = new ShowTreeVisitor();
+        result.accept(visitor, 0);
           
-          SemanticAnalyzer visitor2 = new SemanticAnalyzer();
-          result.accept(visitor2, 0);
-          visitor2.table.exitScope("global scope", 0);
-          System.out.println("Parsing completed.");
+        SemanticAnalyzer visitor2 = new SemanticAnalyzer();
+        result.accept(visitor2, 0);
+        visitor2.table.exitScope("global scope", 0);
+        outputFile.close();
+        System.setOut(console);
+        System.out.println("Syntax tree and Symbol table saved to " + baseFile + ".sym");
+      }
+      //displays syntax tree 
+      else if (saveTree && result != null) {
+        PrintStream outputFile = new PrintStream(new FileOutputStream(baseFile + ".abs"));
+        System.setOut(outputFile);
+        System.out.println("The abstract syntax tree is:");
+        AbsynVisitor visitor = new ShowTreeVisitor();
+        result.accept(visitor, 0);
+        outputFile.close();
+        System.setOut(console);
+        System.out.println("Syntax tree saved to " + baseFile + ".abs");
       
-        }
-        //displays syntax tree 
-        else if (saveTree && result != null) {
-          System.out.println("The abstract syntax tree is:");
-          AbsynVisitor visitor = new ShowTreeVisitor();
-          result.accept(visitor, 0);
-          System.out.println("Parsing completed.");
-      
-        }
-        else {
-            System.out.println("Bad news: -s or -a required to run syntax tree and/or semantic analysis :(");
-        }
+      }
+
     } catch (Exception e) {
       e.printStackTrace();
     }
